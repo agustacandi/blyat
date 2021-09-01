1e224554a42dff03606c1269b4948ef420bcd337
Candi Agusta <mugiwaraagusta@gmail.com>
2021-09-01 14:29:18 +0700

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import Aos from 'aos';
+import Navbar from '../components/organisms/Navbar';
+import MainBanner from '../components/organisms/MainBanner';
+import TransactionsStep from '../components/organisms/TransactionsStep';
+import FeaturedGames from '../components/organisms/FeaturedGames';
+import Reached from '../components/organisms/Reached';
+import Story from '../components/organisms/Story';
+import Footer from '../components/organisms/Footer';
+
+export default function Home() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <MainBanner />
+      <TransactionsStep />
+      <FeaturedGames />
+      <Reached />
+      <Story />
+      <Footer />
+    </>
+  );
+}
